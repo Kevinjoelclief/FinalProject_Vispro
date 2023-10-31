5834b4c542ab81d85c178fcc4d15ab87832c452c
Kjoell29 <kevinjoelclief57@gmail.com>
2023-10-31 15:23:56 +0800

    Final Project Vispro

--- DONDONApp/FormsLogin.Designer.cs
+++ DONDONApp/FormsLogin.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormsLogin));
-            this.label10 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label9 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label8 = new System.Windows.Forms.Label();
@@ -48,47 +46,28 @@
             this.email1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Nirmala UI Semilight", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(213, 609);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(44, 19);
-            this.label10.TabIndex = 36;
-            this.label10.Text = "Apple";
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(217, 575);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(40, 31);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 35;
-            this.pictureBox3.TabStop = false;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Nirmala UI Semilight", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(120, 609);
+            this.label9.Location = new System.Drawing.Point(116, 396);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(52, 19);
+            this.label9.Size = new System.Drawing.Size(37, 12);
             this.label9.TabIndex = 34;
             this.label9.Text = "Google";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(124, 575);
+            this.pictureBox2.Location = new System.Drawing.Point(118, 374);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(32, 31);
+            this.pictureBox2.Size = new System.Drawing.Size(25, 20);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 33;
             this.pictureBox2.TabStop = false;
@@ -97,35 +76,39 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Nirmala UI", 7F, System.Drawing.FontStyle.Bold);
-            this.label8.Location = new System.Drawing.Point(152, 539);
+            this.label8.Location = new System.Drawing.Point(101, 350);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(82, 19);
+            this.label8.Size = new System.Drawing.Size(59, 12);
             this.label8.TabIndex = 32;
             this.label8.Text = "Signin with";
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Location = new System.Drawing.Point(240, 548);
+            this.panel2.Location = new System.Drawing.Point(160, 356);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(162, 10);
+            this.panel2.Size = new System.Drawing.Size(108, 6);
             this.panel2.TabIndex = 31;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(0, 548);
+            this.panel1.Location = new System.Drawing.Point(0, 356);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(146, 10);
+            this.panel1.Size = new System.Drawing.Size(97, 6);
             this.panel1.TabIndex = 30;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Nirmala UI", 7F, System.Drawing.FontStyle.Bold);
-            this.label7.Location = new System.Drawing.Point(243, 478);
+            this.label7.Location = new System.Drawing.Point(162, 311);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(55, 19);
+            this.label7.Size = new System.Drawing.Size(38, 12);
             this.label7.TabIndex = 29;
             this.label7.Text = "Signup";
             // 
@@ -133,9 +116,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Nirmala UI", 7F, System.Drawing.FontStyle.Bold);
-            this.label6.Location = new System.Drawing.Point(86, 478);
+            this.label6.Location = new System.Drawing.Point(57, 311);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(162, 19);
+            this.label6.Size = new System.Drawing.Size(112, 12);
             this.label6.TabIndex = 28;
             this.label6.Text = "Don\'t have an account?";
             // 
@@ -144,9 +128,10 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Nirmala UI", 6F, System.Drawing.FontStyle.Bold);
             this.label5.ForeColor = System.Drawing.Color.SandyBrown;
-            this.label5.Location = new System.Drawing.Point(237, 326);
+            this.label5.Location = new System.Drawing.Point(158, 212);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(104, 15);
+            this.label5.Size = new System.Drawing.Size(72, 11);
             this.label5.TabIndex = 26;
             this.label5.Text = "Forgot password?";
             // 
@@ -154,36 +139,40 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Nirmala UI", 7F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(73, 326);
+            this.label4.Location = new System.Drawing.Point(49, 212);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(73, 19);
+            this.label4.Size = new System.Drawing.Size(48, 12);
             this.label4.TabIndex = 25;
             this.label4.Text = "Password";
             // 
             // pass1
             // 
-            this.pass1.Location = new System.Drawing.Point(57, 344);
+            this.pass1.Location = new System.Drawing.Point(38, 224);
+            this.pass1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pass1.Multiline = true;
             this.pass1.Name = "pass1";
-            this.pass1.Size = new System.Drawing.Size(279, 37);
+            this.pass1.Size = new System.Drawing.Size(187, 25);
             this.pass1.TabIndex = 24;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Nirmala UI", 7F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(73, 232);
+            this.label3.Location = new System.Drawing.Point(49, 151);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(133, 19);
+            this.label3.Size = new System.Drawing.Size(90, 12);
             this.label3.TabIndex = 23;
             this.label3.Text = "Email or username";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(205, 8);
+            this.pictureBox1.Location = new System.Drawing.Point(137, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(197, 153);
+            this.pictureBox1.Size = new System.Drawing.Size(131, 99);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 22;
             this.pictureBox1.TabStop = false;
@@ -192,9 +181,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(26, 116);
+            this.label2.Location = new System.Drawing.Point(17, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 45);
+            this.label2.Size = new System.Drawing.Size(90, 30);
             this.label2.TabIndex = 21;
             this.label2.Text = "Back to";
             // 
@@ -202,18 +192,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(26, 68);
+            this.label1.Location = new System.Drawing.Point(17, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 48);
+            this.label1.Size = new System.Drawing.Size(126, 32);
             this.label1.TabIndex = 20;
             this.label1.Text = "Wellcome";
             // 
             // email1
             // 
-            this.email1.Location = new System.Drawing.Point(57, 254);
+            this.email1.Location = new System.Drawing.Point(38, 165);
+            this.email1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.email1.Multiline = true;
             this.email1.Name = "email1";
-            this.email1.Size = new System.Drawing.Size(279, 36);
+            this.email1.Size = new System.Drawing.Size(187, 25);
             this.email1.TabIndex = 19;
             this.email1.TextChanged += new System.EventHandler(this.email1_TextChanged);
             // 
@@ -221,9 +213,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Orange;
             this.button1.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(90, 407);
+            this.button1.Location = new System.Drawing.Point(60, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(208, 51);
+            this.button1.Size = new System.Drawing.Size(139, 33);
             this.button1.TabIndex = 37;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
@@ -235,12 +228,10 @@
             // 
             // FormsLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(403, 710);
+            this.ClientSize = new System.Drawing.Size(269, 456);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label8);
@@ -256,10 +247,10 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.email1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormsLogin";
             this.Text = "FormsLogin";
             this.Load += new System.EventHandler(this.FormsLogin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -268,9 +259,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label8;
